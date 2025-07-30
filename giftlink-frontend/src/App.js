7786c17650f5fa286bb56f9b4c3e172f18e63c38
Shashwatt699 <shashwatsaxena454@gmail.com>
2025-07-30 12:26:53 -0400

    Search Functionality

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -7,22 +7,24 @@ import Navbar from './components/Navbar/Navbar';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import DetailsPage from './components/DetailsPage/DetailsPage';
+import SearchPage from './components/SearchPage/SearchPage';
 
 function App() {
 
-  return (
-    <>
-        <Navbar/>
-        <Routes>
-          {/* the final code will not pass the products to every page, but each page will call the server API */}
-          <Route path="/" element={<MainPage />} />
-          <Route path="/app" element={<MainPage />} />
-          <Route path="/app/login" element={<LoginPage />} />
-          <Route path="/app/register" element={<RegisterPage />} />
-          <Route path="/app/product/:productId" element={<DetailsPage />} /> 
-        </Routes>
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                {/* the final code will not pass the products to every page, but each page will call the server API */}
+                <Route path="/" element={<MainPage />} />
+                <Route path="/app" element={<MainPage />} />
+                <Route path="/app/login" element={<LoginPage />} />
+                <Route path="/app/register" element={<RegisterPage />} />
+                <Route path="/app/product/:productId" element={<DetailsPage />} />
+                <Route path="/app/search" element={<SearchPage />} />
+            </Routes>
         </>
-  );
+    );
 }
 
 export default App;
